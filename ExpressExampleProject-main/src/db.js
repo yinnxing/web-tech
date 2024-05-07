1cdf67e1f8585270a4e9b0320e42b88b7ce8dd87
yinnxing <61074096+yinnxing@users.noreply.github.com>
2024-05-07 15:24:41 +0700

    Delete ExpressExampleProject-main directory

--- ExpressExampleProject-main/src/db.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-
-const {MongoClient} = require("mongodb");
-const client = new MongoClient(process.env.MONGODB_CONNECT_STRING, {
-    useUnifiedTopology: true,
-});
-const connection = client.connect();
-module.exports = connection;
